bdb740d98cb248f9e7899b67ffcb5f7701b9d80e
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-06-30 22:58:41 +0500

    roles

--- register.php
+++ register.php
@@ -63,6 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
+            <a href="dashboard.php" class="btn btn-link">На главную</a>
         </form>
     </div>
 </body>
